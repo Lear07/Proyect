bd978a3a545111b0a18e772545ce53f3e1b7b75c
Lear07 <up240921@alumnos.upa.edu.mx>
2026-03-02 20:09:47 -0600

    add fetch getPokemons

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  useEffect(() => {
+    console.log("Entre entre pantalla");
+    getPokemons(); // llamamos
+  }, []);
+  const getPokemons = async () => { // Funcion flecha
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+    const response = await fetch(URL, { // fetch (Traer cambios)
+      method: "Get", //POST - PUT - DELETE
+    });
+    console.log(response);
+    const data = await response.json(); // Lo convertimos a json
+    console.log(data.results);
+  };
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>Que onda</Text>
     </View>
   );
 }
